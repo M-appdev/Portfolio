9c1a54835dc3c382bf1d22dc9d101019e27abf51
Milan Krunic <milankrunic12@gmail.com>
2022-03-08 10:26:12 +0100

    Contact me !!

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react"
 import "./contact.scss"
 
 const Contact = () => {
-
+    const [showThankYou, setShowThankYou] = useState(false);
 
 
     return (
@@ -9,9 +10,29 @@ const Contact = () => {
 
             <div className="title">
                 <h1 className="page-title">Contact me!</h1>
-                <p className="page-description">Feel free to contact me, I usually respond in a couple of minutes.</p>
+                <p className="page-description">Feel free to contact me, I usually respond within a couple of minutes.</p>
             </div>
+
+
+            <form action="https://formsubmit.co/milankrunic12@gmail.com" method="POST" onSubmit={() => alert("Message sent! I will respond asap!")}>
+                <input type="hidden" name="_captcha" value="false" />
+                <input type="hidden" name="_subject" value="New email from website!" />
+                <input type="hidden" name="_next" value="http://localhost:3000/#Home" />
+
+                <input className="form name" type="text" name="name" required placeholder="Your name" />
+                <input className="form email" type="email" name="email" required placeholder="Email" />
+                <input className="form subject" type="subject" name="subject" placeholder="Subject" />
+
+                <input className=" form message" type="message" name="message" required placeholder="Message " />
+
+                <button className="myButton" type="submit" >Send</button>
+            </form>
+
+
+
         </div>
+
+
     )
 }
 export default Contact

d3cd1f579d76451333c2124a4a70d0bc33eaa0ca
Milan Krunic <milankrunic12@gmail.com>
2022-03-09 14:43:52 +0100

    Dynamic landing

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -5,13 +5,15 @@ const Landing = () => {
 
 
     return (
-       <div className="landing page">
+        <div className="landing page">
+            <div className="align-text">
+                <h1 className="milan-krunic">Milan Krunic</h1>
+                <p className="description">Junior Web Developer</p>
+            </div>
 
-           <h1 className="milan-krunic">Milan Krunic</h1>
-           <p className="description">Junior Web Developer</p>
 
 
-       </div>
+        </div>
     )
 }
 export default Landing

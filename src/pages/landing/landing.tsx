0f9138b614b75304327641fe447881d92ec462da
Milan Krunic <milankrunic12@gmail.com>
2022-03-14 09:39:36 +0100

    Made landing page dynamic

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -6,12 +6,12 @@ const Landing = () => {
 
     return (
         <div className="landing page">
+
             <div className="align-text">
                 <h1 className="milan-krunic">Milan Krunic</h1>
+                {/* <div className="break"></div> */}
                 <p className="description">Junior Web Developer</p>
             </div>
-
-
 
         </div>
     )
--- src/pages/work-together/work-together.tsx
+++ src/pages/work-together/work-together.tsx
@@ -9,10 +9,7 @@ const WorkTogether = () => {
             <div className="container">
                 <h1 className="title ">Let's Work Together!</h1>
                 <p className="description">I am always up for a challange!</p>
-            </div>
-
-            <div className="button-container">
-                <a href="#Contact" className="myButton">Send me a Message</a>
+            </div> <a href="#Contact" className="myButton">Send me a Message</a><div>
             </div>
 
         </div>

603551c8c4a304a763e2772283b126d64f3d4e56
Milan Krunic <milankrunic12@gmail.com>
2022-03-15 09:01:48 +0100

    moved scroll logic to separate file

---
+++ src/side-bar/amount-scrolled.tsx
@@ -0,0 +1,65 @@
+
+
+export default function amountscrolled() {
+
+    var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight
+
+    var D = document;
+
+    var docheight =Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    var trackLength = docheight - winheight
+    var pctScrolled = Math.floor(scrollTop / trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+    if (pctScrolled < 17) { //home
+        let nextElement = document.getElementById('about');
+        if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+
+        let element = document.getElementById('home');
+        if (element) (element as HTMLParagraphElement).style.color = "#007bff"
+
+        let lastElement = document.getElementById('about');
+        if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+    } else if (pctScrolled > 17 && pctScrolled < 38) { //about
+        let nextElement = document.getElementById('home');
+        if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+
+        let element = document.getElementById('about');
+        if (element) (element as HTMLParagraphElement).style.color = "#007bff"
+
+        let lastElement = document.getElementById('skills');
+        if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+    }
+    else if (pctScrolled > 38 && pctScrolled < 61) { //skills
+        let nextElement = document.getElementById('about');
+        if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+
+        let element = document.getElementById('skills');
+        if (element) (element as HTMLParagraphElement).style.color = "#007bff"
+
+        let lastElement = document.getElementById('portfolio');
+        if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+    }
+    else if (pctScrolled > 61 && pctScrolled < 92) { //portfolio
+        let nextElement = document.getElementById('skills');
+        if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+
+        let element = document.getElementById('portfolio');
+        if (element) (element as HTMLParagraphElement).style.color = "#007bff"
+
+        let lastElement = document.getElementById('contact');
+        if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+    }
+    else if (pctScrolled > 92 && pctScrolled <= 100) {//contact
+        let nextElement = document.getElementById('portfolio');
+        if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+
+        let element = document.getElementById('contact');
+        if (element) (element as HTMLParagraphElement).style.color = "#007bff"
+
+
+    }
+}
--- src/side-bar/side-bar.tsx
+++ src/side-bar/side-bar.tsx
@@ -1,76 +1,27 @@
 
 import "./side-bar.scss"
+import amountScrolled from "./amount-scrolled"
+
 const SideBar = () => {
 
     let myPicture = require("../assets/images/MugShot.jpg")
 
-    function getDocHeight() {
-        var D = document;
-        return Math.max(
-            D.body.scrollHeight, D.documentElement.scrollHeight,
-            D.body.offsetHeight, D.documentElement.offsetHeight,
-            D.body.clientHeight, D.documentElement.clientHeight
-        )
-    }
+    const setLinkColors=(link: string)=> {
 
-    function amountscrolled() {
-        var winheight = window.innerHeight || (document.documentElement || document.body).clientHeight
-        var docheight = getDocHeight()
-        var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
-        var trackLength = docheight - winheight
-        var pctScrolled = Math.floor(scrollTop / trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
-        if (pctScrolled < 17) { //home
-            let nextElement = document.getElementById('about');
-            if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+        const navLinks = document.getElementsByClassName('nav-link');
 
-            let element = document.getElementById('home');
-            if (element) (element as HTMLParagraphElement).style.color = "#007bff"
-
-            let lastElement = document.getElementById('about');
-            if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
-        } else if (pctScrolled > 17 && pctScrolled < 38) { //about
-            let nextElement = document.getElementById('home');
-            if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
-
-            let element = document.getElementById('about');
-            if (element) (element as HTMLParagraphElement).style.color = "#007bff"
-
-            let lastElement = document.getElementById('skills');
-            if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
+        for (let i = 0; i < navLinks.length; i++) {
+            const slide = navLinks[i] as HTMLElement;
+            slide.style.color = "rgba(232, 232, 232, 0.95)";
         }
-        else if (pctScrolled > 38 && pctScrolled < 61) { //skills
-            let nextElement = document.getElementById('about');
-            if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
 
-            let element = document.getElementById('skills');
-            if (element) (element as HTMLParagraphElement).style.color = "#007bff"
+        let element = document.getElementById(link);
+        if (element) (element as HTMLParagraphElement).style.color = "#007bff"
 
-            let lastElement = document.getElementById('portfolio');
-            if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
-        }
-        else if (pctScrolled > 61 && pctScrolled < 92) { //portfolio
-            let nextElement = document.getElementById('skills');
-            if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
-
-            let element = document.getElementById('portfolio');
-            if (element) (element as HTMLParagraphElement).style.color = "#007bff"
-
-            let lastElement = document.getElementById('contact');
-            if (lastElement) (lastElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
-        }
-        else if (pctScrolled > 92 && pctScrolled <= 100) {//contact
-            let nextElement = document.getElementById('portfolio');
-            if (nextElement) (nextElement as HTMLParagraphElement).style.color = "rgba(232, 232, 232, 0.95)"
-
-            let element = document.getElementById('contact');
-            if (element) (element as HTMLParagraphElement).style.color = "#007bff"
-
-
-        }
     }
 
     window.addEventListener("scroll", function () {
-        amountscrolled()
+        amountScrolled()
     }, false)
 
 
@@ -98,17 +49,3 @@ const SideBar = () => {
 export default SideBar
 
 
-
-function setLinkColors(link: string) {
-
-    const navLinks = document.getElementsByClassName('nav-link');
-
-    for (let i = 0; i < navLinks.length; i++) {
-        const slide = navLinks[i] as HTMLElement;
-        slide.style.color = "rgba(232, 232, 232, 0.95)";
-    }
-
-    let element = document.getElementById(link);
-    if (element) (element as HTMLParagraphElement).style.color = "#007bff"
-
-}
